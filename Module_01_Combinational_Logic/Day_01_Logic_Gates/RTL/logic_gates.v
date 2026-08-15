276d7857ffa68aac2a07d5622a2fbad155c547a9
ramsaipolaki-sys <ramsaipolaki@gmail.com>
2026-08-15 11:30:31 +0530

    Add Verilog module for basic logic gates

---
+++ Module_01_Combinational_Logic/Day_01_Logic_Gates/RTL/logic_gates.v
@@ -0,0 +1,22 @@
+module logic_gates (
+    input  a,
+    input  b,
+
+    output and_y,
+    output or_y,
+    output not_y,
+    output nand_y,
+    output nor_y,
+    output xor_y,
+    output xnor_y
+);
+
+assign and_y  = a & b;
+assign or_y   = a | b;
+assign not_y  = ~a;
+assign nand_y = ~(a & b);
+assign nor_y  = ~(a | b);
+assign xor_y  = a ^ b;
+assign xnor_y = ~(a ^ b);
+
+endmodule
