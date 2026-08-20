c523bd7c2c65bfe3b0d9a55d608b1e18f4e1793c
ramsaipolaki-sys <ramsaipolaki@gmail.com>
2026-08-20 14:49:04 +0530

    Add full subtractor RTL

---
+++ Module_01_Combinational_Logic/Day_02_Adders_Subtractors/Full_Subtractor/RTL/full_subtractor.v
@@ -0,0 +1,13 @@
+  module full_subtractor (
+    input  a,
+    input  b,
+    input  bin,
+    output difference,
+    output borrow
+);
+
+assign difference = a ^ b ^ bin;
+
+assign borrow = (~a & b) | (~a & bin) | (b & bin);
+
+endmodule
