573bde7d59ebe6781b22c623967ecee6d2e4789e
ramsaipolaki-sys <ramsaipolaki@gmail.com>
2026-08-21 10:47:14 +0530

    Add 4x1 multiplexer module in Verilog

---
+++ Module_01_Combinational_Logic/Day_03_MUX_DEMUX/4x1_MUX/RTL/mux_4x1.v
@@ -0,0 +1,15 @@
+module mux_4x1 (
+    input  a,
+    input  b,
+    input  c,
+    input  d,
+    input  [1:0] sel,
+    output y
+);
+
+assign y = (sel == 2'b00) ? a :
+           (sel == 2'b01) ? b :
+           (sel == 2'b10) ? c :
+                            d;
+
+endmodule
