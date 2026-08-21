9e88293d1615884dfdbf41e3f51c4d8e4743b96c
ramsaipolaki-sys <ramsaipolaki@gmail.com>
2026-08-21 10:49:11 +0530

    Add 1x4 demultiplexer implementation

---
+++ Module_01_Combinational_Logic/Day_03_MUX_DEMUX/1x4_DEMUX/RTL/demux_1x4.v
@@ -0,0 +1,20 @@
+module demux_1x4 (
+    input din,
+    input [1:0] sel,
+    output reg [3:0] y
+);
+
+always @(*) begin
+
+    y = 4'b0000;
+
+    case (sel)
+        2'b00: y[0] = din;
+        2'b01: y[1] = din;
+        2'b10: y[2] = din;
+        2'b11: y[3] = din;
+    endcase
+
+end
+
+endmodule
